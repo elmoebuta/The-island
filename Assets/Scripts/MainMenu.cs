28f2bafca7a871742ed1e7586d721c33368ddd72
Sebastian Trujillo <74939920+SebastianTrujillo21@users.noreply.github.com>
2023-09-06 00:12:01 -0500

    MainMenu Finish

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -2,20 +2,35 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using TMPro;
 
 public class MainMenu : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [Header("Volume Settings")]
+    [SerializeField]
+    private TMP_Text volumeTextValue = null;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    [SerializeField]
+    private Slider volumeSlider = null;
+
+    [SerializeField]
+    private GameObject confirmationPrompt = null;
+
+    [SerializeField]
+    private float defaultVolumeValue = 1.0f;
+
+    [Header("Gameplay Settings")]
+    [SerializeField]
+    private TMP_Text controllerSenTextValue = null;
+
+    [SerializeField]
+    private Slider controllerSenSlider = null;
+
+    [SerializeField]
+    private int defaultSen = 4;
+
+    public int mainControllerSen = 4;
 
     public void CargarEscenaJuego(string nombreEscena)
     {
@@ -30,5 +45,53 @@ public class MainMenu : MonoBehaviour
             Application.Quit();
         #endif
 
+    }
+
+    public void setVolumen(float volumen)
+    {
+        AudioListener.volume = volumen;
+        volumeTextValue.text = volumen.ToString("0.0");
+    }
+
+    public void VolumeApply()
+    {
+        StartCoroutine(ConfirmationBox());
+    }
+
+    public IEnumerator ConfirmationBox()
+    {
+        confirmationPrompt.SetActive(true);
+        yield return new WaitForSeconds(2);
+        confirmationPrompt.SetActive(false);
+    }
+
+    public void ResetButton(string MenuType)
+    {
+        if(MenuType == "Audio")
+        {
+            AudioListener.volume = defaultVolumeValue;
+            volumeSlider.value = defaultVolumeValue;
+            volumeTextValue.text = defaultVolumeValue.ToString("0.0");
+            VolumeApply();
+        }
+
+        if (MenuType == "GamePlay")
+        {
+            controllerSenTextValue.text = defaultSen.ToString("0");
+            controllerSenSlider.value = defaultSen;
+            mainControllerSen = defaultSen;
+            GameplayApply();
+        }
+    }
+
+    public void SetControllerSen(float sensitivity)
+    {
+        mainControllerSen = Mathf.RoundToInt(sensitivity);
+        controllerSenTextValue.text = sensitivity.ToString("0");
+    }
+
+    public void GameplayApply()
+    {
+        StartCoroutine(ConfirmationBox());
     }
 }
