61ca39ac9f7aaee2954cbf26607a4450a3c712ca
elmoebuta <u201726329@upc.edu.pe>
2023-09-14 12:55:46 -0500

    comer
    comer

--- Assets/Scripts/AnimalMovement.cs
+++ Assets/Scripts/AnimalMovement.cs
@@ -14,6 +14,7 @@ public class AnimalMovement : MonoBehaviour
     private Vector3 moveDirection; // Dirección de movimiento actual
     private Vector2 center = new Vector2(250f, 250f); // Centro de la circunferencia
     private float maxRadius = 210f; // Radio máximo de la circunferencia
+    public bool isActive = true;
 
     private void Start()
     {
@@ -28,51 +29,58 @@ public class AnimalMovement : MonoBehaviour
         // Actualizar el temporizador
         timer += Time.deltaTime;
 
-        if (isWalking)
+        if (isActive)
         {
-            // Si ha caminado el tiempo suficiente, cambia a la animación de estar quieto
-            if (timer >= walkTime)
+            if (isWalking)
             {
-                animator.SetInteger("GoIdle", 1);
-                animator.SetInteger("GoWalk", 0);
-                isWalking = false;
-                timer = 0f; // Reiniciar el temporizador
-                rigidbody.velocity = Vector3.zero; // Detener el movimiento
+                // Si ha caminado el tiempo suficiente, cambia a la animación de estar quieto
+                if (timer >= walkTime)
+                {
+                    animator.SetInteger("GoIdle", 1);
+                    animator.SetInteger("GoWalk", 0);
+                    isWalking = false;
+                    timer = 0f; // Reiniciar el temporizador
+                    rigidbody.velocity = Vector3.zero; // Detener el movimiento
+                }
+                else
+                {
+                    // Mover al animal hacia adelante mientras camina
+                    RotateTowardsDirection(moveDirection);
+                    rigidbody.velocity = moveDirection * moveSpeed;
+                    // Rotar gradualmente hacia la dirección de movimiento
+
+                    // Limitar el movimiento dentro de la circunferencia
+                    Vector2 currentPos = new Vector2(transform.position.x, transform.position.z);
+                    float distanceToCenter = Vector2.Distance(currentPos, center);
+
+                    if (distanceToCenter > maxRadius)
+                    {
+                        // El animal está fuera de la circunferencia, cambiar de dirección
+                        Vector2 directionToCenter = (center - currentPos).normalized;
+                        moveDirection = new Vector3(directionToCenter.x, 0f, directionToCenter.y);
+                        RotateTowardsDirection(moveDirection);
+                    }
+                }
             }
             else
             {
-                // Mover al animal hacia adelante mientras camina
-                RotateTowardsDirection(moveDirection);
-                rigidbody.velocity = moveDirection * moveSpeed;
-                // Rotar gradualmente hacia la dirección de movimiento
-
-                // Limitar el movimiento dentro de la circunferencia
-                Vector2 currentPos = new Vector2(transform.position.x, transform.position.z);
-                float distanceToCenter = Vector2.Distance(currentPos, center);
-
-                if (distanceToCenter > maxRadius)
+                // Si ha estado quieto el tiempo suficiente, cambia a la animación de caminar
+                if (timer >= idleTime)
                 {
-                    // El animal está fuera de la circunferencia, cambiar de dirección
-                    Vector2 directionToCenter = (center - currentPos).normalized;
-                    moveDirection = new Vector3(directionToCenter.x, 0f, directionToCenter.y);
-                    RotateTowardsDirection(moveDirection);
+                    animator.SetInteger("GoIdle", 0);
+                    animator.SetInteger("GoWalk", 1);
+                    isWalking = true;
+                    timer = 0f; // Reiniciar el temporizador
+                    rigidbody.velocity = Vector3.zero; // Detener el movimiento
+                                                       // Inicializar una nueva dirección de movimiento aleatoria
+                    SetRandomMoveDirection();
                 }
             }
+
+
         }
-        else
-        {
-            // Si ha estado quieto el tiempo suficiente, cambia a la animación de caminar
-            if (timer >= idleTime)
-            {
-                animator.SetInteger("GoIdle", 0);
-                animator.SetInteger("GoWalk", 1);
-                isWalking = true;
-                timer = 0f; // Reiniciar el temporizador
-                rigidbody.velocity = Vector3.zero; // Detener el movimiento
-                // Inicializar una nueva dirección de movimiento aleatoria
-                SetRandomMoveDirection();
-            }
-        }
+
+       
     }
 
     // Establecer una dirección de movimiento aleatoria
--- Assets/Scripts/Interactor.cs
+++ Assets/Scripts/Interactor.cs
@@ -1,60 +1,98 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
+
+
+
 
 public class Interactor : MonoBehaviour
 {
+
     public float interactionDistance = 3f; // Distancia máxima de interacción.
     private Animator duckAnimator; // Animator del pato.
     private bool isEating = false; // Indica si el pato está comiendo actualmente.
-    public AnimalMovement animalMovement; // Referencia al script AnimalMovement.
+    private AnimalMovement animalMovement; // Referencia al script AnimalMovement.
+    private bool hasInteracted = false;
 
     private void Start()
     {
         duckAnimator = GetComponent<Animator>();
+        animalMovement = GetComponent<AnimalMovement>();
     }
 
     private void Update()
     {
         // Detecta si el personaje principal está dentro de la distancia de interacción.
         Collider[] colliders = Physics.OverlapSphere(transform.position, interactionDistance);
-        foreach (Collider col in colliders)
+
+        if (isEating == false) // Verifica que no se haya interactuado previamente.
         {
-            if (col.CompareTag("Player"))
+            foreach (Collider col in colliders)
             {
-                // Calcula la distancia entre el pato y el jugador.
-                float distance = Vector3.Distance(transform.position, col.transform.position);
-
-                // Si el jugador presiona la tecla de interacción (por ejemplo, "F") y está dentro de la distancia de interacción, ejecuta el cuack del pato.
-                if (Input.GetKeyDown(KeyCode.F) && distance <= interactionDistance)
+                if (col.CompareTag("Player"))
                 {
-                    Debug.Log("Pato: Cuack");
+                    // Calcula la distancia entre el pato y el jugador.
+                    float distance = Vector3.Distance(transform.position, col.transform.position);
 
-                    // Cambiar la transición a "GoEat".
-                    duckAnimator.SetInteger("GoEat", 1);
-                    isEating = true;
+                    // Si el jugador presiona la tecla de interacción (por ejemplo, "F") y está dentro de la distancia de interacción, ejecuta el cuack del pato.
+                    if (Input.GetKeyDown(KeyCode.F) && distance <= interactionDistance)
+                    {
+                        //Debug.Log("Pato: Cuack");
 
-                    // Desactivar el movimiento del pato.
-                   // animalMovement.isActive = false;
+                        // Cambiar la transición a "GoEat".
+                        duckAnimator.SetInteger("GoEat", 1);
+                        
+                        
+                        duckAnimator.SetInteger("GoWalk", 0);
+                        duckAnimator.SetInteger("GoIdle", 0);
+                        
+                        animalMovement.isActive = false;
+                        isEating = true;
+                         // Marca que la interacción ya ha ocurrido.
+
+                      //  Debug.Log("Interacción realizada");
+
+                        // Sal del bucle foreach.
+                        
+                        // Desactivar el movimiento del pato.
+                    }
+
+                    Debug.Log(isEating);
                 }
             }
         }
 
+
         // Comprobar si el pato está en modo "Eat" para controlar el retorno al movimiento.
-        if (isEating)
+        if (isEating==true)
         {
             // Si la animación de "Eat" ha terminado, volver al movimiento.
             AnimatorStateInfo stateInfo = duckAnimator.GetCurrentAnimatorStateInfo(0);
-            if (stateInfo.IsName("Eat") && stateInfo.normalizedTime >= 1f)
+            if (stateInfo.IsName("eat"))
             {
-                // Reiniciar la variable "GoEat".
-                duckAnimator.SetInteger("GoEat", 0);
-                isEating = false;
+                Debug.Log("Estado actual: Eat");
+                Debug.Log("Normalized Time: " + stateInfo.normalizedTime);
 
-                // Reactivar el movimiento del pato.
-               // animalMovement.isActive = true;
+                if (stateInfo.normalizedTime >= 0.9f)
+                {
+                    Debug.Log("Final de la animación Eat alcanzado.");
+
+                    // Reiniciar la variable "GoEat".
+                    duckAnimator.SetInteger("GoEat", 0);
+                    duckAnimator.SetInteger("GoWalk", 1);
+                    duckAnimator.SetInteger("GoIdle", 1);
+                    animalMovement.isActive = true;
+                    
+
+                    isEating = false;
+
+                    // Reactivar el movimiento del pato.
+                }
+            }
+            else
+            {
+                Debug.Log("Estado actual no es Eat.");
             }
         }
     }
 }
-
-
-
