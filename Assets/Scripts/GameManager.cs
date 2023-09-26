008565f96b07a161c8ab740d3c4e99e34d1d9962
elmoebuta <u201726329@upc.edu.pe>
2023-09-26 02:20:42 -0500

    roca destruye

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,7 @@
+using UnityEngine;
+
+public static class GameManager
+{
+    public static int contadorAzul = 0;
+    public static int contadorRosado = 0;
+}
---
+++ Assets/Scripts/Rock.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rock : MonoBehaviour
+{
+    public string color = "Red"; // Variable pública para el color del objeto.
+    public float destroyRadius = 5f; // Radio de destrucción cuando el jugador se acerca.
+    public string playerTag = "Player"; // Tag del jugador.
+
+    private void Start()
+    {
+        // Agrega código adicional de inicialización si es necesario.
+    }
+
+    private void Update()
+    {
+        // Imprime la posición del objeto "Rock" y el color.
+        Debug.Log("Posición de Rock: " + transform.position);
+
+        // Busca todos los objetos con el tag "Player" en el rango de destrucción.
+        Collider[] colliders = Physics.OverlapSphere(transform.position, destroyRadius);
+
+        foreach (Collider col in colliders)
+        {
+            if (col.CompareTag(playerTag))
+            {
+                // Incrementa el contador correspondiente según el color.
+                if (color == "blue")
+                {
+                    GameManager.contadorAzul++;
+                }
+                else if (color == "pink")
+                {
+                    GameManager.contadorRosado++;
+                }
+
+                // Destruye el objeto "Rock" si el jugador está en el rango.
+                Destroy(gameObject);
+                break; // Sal del bucle ya que el objeto "Rock" se ha destruido.
+            }
+        }
+    }
+}
